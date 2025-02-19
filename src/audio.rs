8db1537010e99ea49f3d2b5f68310983511a570c
AwfullyMatt <the.xeronite@gmail.com>
2025-02-19 10:31:42 -0700

    Update Audio Plugin

--- src/audio.rs
+++ src/audio.rs
@@ -16,15 +16,26 @@ impl Plugin for InternalAudioPlugin {
                 (
                     evr_play_metronome_audio,
                     evr_pause_metronome_audio,
+                    evr_stop_metronome_audio,
                     update_note_timers,
                 ),
             )
             .init_resource::<CurrentSong>()
             .init_resource::<MetronomeAudioChannel>()
+            .init_state::<MetronomeState>()
             .add_audio_channel::<MetronomeAudioChannel>()
             .add_event::<PlayMetronomeAudio>()
-            .add_event::<PauseMetronomeAudio>();
+            .add_event::<PauseMetronomeAudio>()
+            .add_event::<StopMetronomeAudio>();
     }
+}
+
+#[derive(States, Default, Clone, Eq, PartialEq, Debug, Hash)]
+pub enum MetronomeState {
+    #[default]
+    Stopped,
+    Paused,
+    Playing,
 }
 
 fn startup(mut commands: Commands, current_song: Res<CurrentSong>) {
@@ -44,6 +55,9 @@ struct PlayMetronomeAudio(Song);
 
 #[derive(Event)]
 struct PauseMetronomeAudio;
+
+#[derive(Event)]
+struct StopMetronomeAudio;
 
 #[derive(Component, Clone)]
 struct Song {
@@ -138,7 +152,7 @@ impl NoteTimer {
 }
 
 #[derive(Component, Clone, Copy, Default, Deref, DerefMut)]
-struct Tempo(f32);
+struct Tempo(f32); // BPM
 impl Tempo {}
 
 #[derive(Component, Clone, Copy, Default)]
@@ -162,29 +176,31 @@ impl NoteKind {
     fn length(&self, audio_info: &AudioInfo) -> Option<f32> {
         use NoteKind::*;
         let bps: f32 = audio_info.tempo.0 / 60.0;
+        let spb: f32 = 1.0 / bps;
+        let top: f32 = audio_info.metre.top as f32;
         match self {
             Whole => {
-                let length = bps * audio_info.metre.top as f32;
+                let length = spb * top;
                 Some(length)
             }
             Half => {
-                let length = (bps * audio_info.metre.top as f32) / 2.0;
+                let length = (spb * top) / 2.0;
                 Some(length)
             }
             Quarter => {
-                let length = (bps * audio_info.metre.top as f32) / 4.0;
+                let length = (spb * top) / 4.0;
                 Some(length)
             }
             Eighth => {
-                let length = (bps * audio_info.metre.top as f32) / 8.0;
+                let length = (spb * top) / 8.0;
                 Some(length)
             }
             Sixteenth => {
-                let length = (bps * audio_info.metre.top as f32) / 16.0;
+                let length = (spb * top) / 16.0;
                 Some(length)
             }
             ThirtySecond => {
-                let length = (bps * audio_info.metre.top as f32) / 32.0;
+                let length = (spb * top) / 32.0;
                 Some(length)
             }
         }
@@ -205,21 +221,44 @@ fn tick_metronome(time: Res<Time>, mut query_metronome: Query<&mut Metronome>) {
 fn evr_play_metronome_audio(
     mut evr_play_metronome_audio: EventReader<PlayMetronomeAudio>,
     metronome_channel: Res<AudioChannel<MetronomeAudioChannel>>,
+    metronome_state: Res<State<MetronomeState>>,
+    mut next_metronome_state: ResMut<NextState<MetronomeState>>,
 ) {
     for ev in evr_play_metronome_audio.read() {
         metronome_channel.play(ev.0.handle.clone());
+        if metronome_state.get() != &MetronomeState::Playing {
+            next_metronome_state.set(MetronomeState::Playing);
+        }
     }
 }
 
 fn evr_pause_metronome_audio(
     mut evr_pause_metronome_audio: EventReader<PauseMetronomeAudio>,
     metronome_channel: Res<AudioChannel<MetronomeAudioChannel>>,
+    metronome_state: Res<State<MetronomeState>>,
+    mut next_metronome_state: ResMut<NextState<MetronomeState>>,
 ) {
     for _ev in evr_pause_metronome_audio.read() {
         metronome_channel.pause();
+        if metronome_state.get() != &MetronomeState::Paused {
+            next_metronome_state.set(MetronomeState::Paused);
+        }
     }
 }
 
+fn evr_stop_metronome_audio(
+    mut evr_pause_metronome_audio: EventReader<StopMetronomeAudio>,
+    metronome_channel: Res<AudioChannel<MetronomeAudioChannel>>,
+    metronome_state: Res<State<MetronomeState>>,
+    mut next_metronome_state: ResMut<NextState<MetronomeState>>,
+) {
+    for _ev in evr_pause_metronome_audio.read() {
+        metronome_channel.pause();
+        if metronome_state.get() != &MetronomeState::Stopped {
+            next_metronome_state.set(MetronomeState::Stopped);
+        }
+    }
+}
 fn update_note_timers(current_song: Res<CurrentSong>, mut query_metronome: Query<&mut Metronome>) {
     if current_song.is_changed() {
         if let Some(song) = &current_song.0 {
